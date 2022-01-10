5fe3c088b3ff4eb767d677f2530b508097406a61
yousungJung <jys01012@gmail.com>
2022-01-10 20:12:07 +0900

    json을 이용한 configure

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { getDomainUri, getDBUri } from './static/configure'
+import { getDomainUri, getDomainPort, getDBUri, getDBPort } from './static/common'
 
 const app = express();
 const helmet = require('helmet'); // node js http secure 모듈
 
-const PORT: String = '13000' // 포트
 const MODE: String = process.argv[2]; // main or dev
-const domain: String = `${getDomainUri(MODE)}:${PORT}`;
-const db: String = getDBUri(MODE);
+const PORT: String = getDomainPort(MODE) // 포트
+const domain: String = `${getDomainUri(MODE)}:${PORT}`; // uri:port
+const db: String = `${getDBUri(MODE)}:${getDBPort(MODE)}`; // uri:port
 
 app.use(helmet()); // helmet의 모든 기능 사용
+
 console.warn(`
     ---------------------------------------------
         Start Server with Condition :: ${MODE}
---
+++ src/static/common.ts
@@ -0,0 +1,38 @@
+// Static variables
+import * as configure from "../../configure.json";
+const DEV_DB_URI:String = configure.develop.database.uri;
+const DEV_DB_PORT: String = configure.develop.database.port;
+const DEV_URI: String = configure.develop.uri;
+const DEV_PORT: String = configure.develop.port;
+
+const MAIN_DB_URI: String = configure.main.database.uri;
+const MAIN_DB_PORT: String = configure.main.database.port;
+const MAIN_URI: String = configure.main.uri;
+const MAIN_PORT: String = configure.main.port;
+
+function getDomainUri(condition: String): String{
+    if(condition === "dev")
+        return DEV_URI;
+    else
+        return MAIN_URI;
+}
+function getDomainPort(condition: String): String{
+    if(condition === "dev")
+        return DEV_PORT;
+    else
+        return MAIN_PORT;
+}
+function getDBUri(condition: String): String{
+    if(condition === "dev")
+        return DEV_DB_URI;
+    else
+        return MAIN_DB_URI;
+}
+
+function getDBPort(condition: String): String{
+    if(condition === "dev")
+        return DEV_DB_PORT;
+    else
+        return MAIN_DB_PORT;
+}
+export { getDomainUri, getDomainPort, getDBUri, getDBPort}
