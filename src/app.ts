a7cc20b07f1edab5afd74a6908e93d71fd8baaf7
Yousung Jung <jys01012@gmail.com>
2022-01-10 18:48:43 +0900

    Helmet 적용 및 mysql 설치 script (#7)
    * readme 추가
    
    * db setting
    
    * helmet 적용 (#6)
    
    conflict 해결

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express, { Request, Response, NextFunction } from 'express'
 import { getDomainUri, getDBUri } from './static/configure'
 
 const app = express();
-const PORT: String = '13000' // 포트
+const helmet = require('helmet'); // node js http secure 모듈
 
+const PORT: String = '13000' // 포트
 const MODE: String = process.argv[2]; // main or dev
 const domain: String = `${getDomainUri(MODE)}:${PORT}`;
 const db: String = getDBUri(MODE);
 
+app.use(helmet()); // helmet의 모든 기능 사용
 console.warn(`
     ---------------------------------------------
         Start Server with Condition :: ${MODE}
