8d29cbe38957c7c4f8a0a526fcab045f7cc9cbf9
yousungJung <jys01012@gmail.com>
2022-01-20 23:08:08 +0900

    mysql init - problem remain

--- src/loaders /index.ts
+++ src/loaders /index.ts
@@ -1,6 +1,8 @@
 import expressLoader from './express';
+import mysqlLoader from './mysql';
 
 export default async ({ expressApp } : {expressApp: any}) => {
+    const mysqlConnection = await mysqlLoader();
     await expressLoader({ app: expressApp });
     console.log('Express Intialized');
 }
---
+++ src/loaders /mysql.ts
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+export default async (): Promise<any> => {
+    const connection = await mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        database : 'test'
+    })
+    
+    //connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+    //return connection.connection.db;
+    return connection.connect();
+  }
