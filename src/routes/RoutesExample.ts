b9c12b9a00645e33b722ddebd6e9cf5c77ebd12b
yousungJung <jys01012@gmail.com>
2022-01-20 16:43:59 +0900

    change dir name

--- src/routes/RoutesExample.ts
+++ src/routes/RoutesExample.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import  ExampleController  from '../controller/ExampleController';
+import  ExampleController  from '../controllers/ExampleController';
 
 const router = Router();
 
