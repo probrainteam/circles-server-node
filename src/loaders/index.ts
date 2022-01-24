6c8c76f2ceb71f6524f1c370b791035609760b88
yousungJung <jys01012@gmail.com>
2022-01-24 17:54:20 +0900

    erase unusing import

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,9 +1,7 @@
-const fs = require('fs');
-const path = require('path');
 const figlet = require('figlet');
 import expressLoader from './express';
 import mysqlLoader from './mysql';
-//import testLoader from './testLoader';
+import testLoader from './testLoader';
 
 export default async ({ expressApp } : {expressApp: any}) => {
     console.log(figlet.textSync('Circles - server', {
@@ -12,14 +10,14 @@ export default async ({ expressApp } : {expressApp: any}) => {
         whitespaceBreak: true
     }));
     try{
-        //const test = await testLoader();
+        const test = await testLoader();
     } catch(err:any){
         console.error(err)
         console.log("no test file... ignore")
     }
     console.warn("MYSQL in Intialize sequence ...")
     await mysqlLoader();
-
+ 
     console.warn("Express in Intialize sequence ...")
     await expressLoader({ app: expressApp });
     console.log('Express Intialized ✅');
