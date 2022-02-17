9d57c3fe708ed85164b77f4dcc12ce4541d31bff
yousungJung <jys01012@gmail.com>
2022-02-18 03:07:13 +0900

    :hammer: 필요없는 statement 삭제, exit 코드 추가

--- init-dev.sh
+++ init-dev.sh
@@ -1,13 +1,11 @@
 # !bin/bash
 if [ -f ".env.local" ]; then
-    if [ -f ".env" ]; then
-        rm -f .env
-    fi
-    cp .env.local .env
-    if [ -f .env ]; then
-        export $(cat .env | sed 's/#.*//g' | xargs) # env file load
-    fi
+    \cp .env.local .env
+    export $(cat .env | sed 's/#.*//g' | xargs) # env file load
     echo "Using development .env"
+else
+    echo "There is no .env.local !!"
+    exit -1
 fi
 
 # docker 실행
