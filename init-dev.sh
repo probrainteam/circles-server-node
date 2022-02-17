a1940aace1323f5de52807db70901ac1b4c3c017
yousungJung <jys01012@gmail.com>
2022-02-17 23:18:36 +0900

    :hammer: mysql docker .env를 통한 실행

---
+++ init-dev.sh
@@ -0,0 +1,18 @@
+# !bin/bash
+if [ -f ".env.local" ]; then
+    if [ -f ".env" ]; then
+        rm -f .env
+    fi
+    cp .env.local .env
+    if [ -f .env ]; then
+        export $(cat .env | sed 's/#.*//g' | xargs) # env file load
+    fi
+    echo "Using development .env"
+fi
+
+# docker 실행
+bash -c "docker-compose -f 'docker-compose.yml' up -d dev_db redis --build"
+docker-compose ps
+# docker 내부 서비스 ping test
+bash -c './wait-for-it.sh -t 60 localhost:${DB_PORT}'
+bash -c './wait-for-it.sh -t 60 localhost:${REDIS_PORT}'
