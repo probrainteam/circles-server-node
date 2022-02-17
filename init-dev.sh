53a53fb9fc28f006ef8719c846d16bfa7acaea24
yousungJung <jys01012@gmail.com>
2022-02-18 03:45:13 +0900

    :technologist: wrapper 작성 및 종료 문구 coloring

--- init-dev.sh
+++ init-dev.sh
@@ -1,4 +1,18 @@
 # !bin/bash
+
+# Wrapper
+function check_status {
+    echo "\033[34m"
+    bash -c "./wait-for-it.sh -t 60 $1"
+    echo "\033[0m"
+    if [ $? ==  0 ]; then
+        echo "\033[32m ...Success\033[0m";
+    else
+        echo "\033[31m >> ...Failed\033[0m";
+        exit -1;
+    fi
+}
+
 if [ -f ".env.local" ]; then
     \cp .env.local .env
     export $(cat .env | sed 's/#.*//g' | xargs) # env file load
@@ -11,6 +25,7 @@ fi
 # docker 실행
 bash -c "docker-compose -f 'docker-compose.yml' up -d --build dev_db redis"
 docker-compose ps
+
 # docker 내부 서비스 ping test
-bash -c './wait-for-it.sh -t 60 localhost:${DB_PORT}'
-bash -c './wait-for-it.sh -t 60 localhost:${REDIS_PORT}'
+check_status "localhost:${DB_PORT}"
+check_status "localhost:${REDIS_PORT}"
