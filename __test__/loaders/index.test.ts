7c63364456a3ac2ed6d3d9cab4356763db556377
yousungJung <jys01012@gmail.com>
2022-02-18 20:58:30 +0900

    :bug: fix yarn test, github pull request action

--- __test__/loaders/index.test.ts
+++ __test__/loaders/index.test.ts
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+import { exit } from "process";
 import request from "supertest"
 import loaders from "../../src/loaders"
+import expressLoader from "../../src/loaders/express"
+import mysqlLoader from "../../src/loaders/redis"
+import redisLoader from "../../src/loaders/express"
+
 describe("Routing test", ()=>{
     test("Get example/test", async ()=>{
         await loaders({ expressApp: app });
@@ -15,5 +20,9 @@ describe("Routing test", ()=>{
         expect(response.statusCode).toBe(404)
     })
 })
+afterAll(done => {
+    // THIS IS HOW YOU CLOSE CONNECTION IN MONGOOSE (mongodb ORM)
+    done();
+});
     
     
