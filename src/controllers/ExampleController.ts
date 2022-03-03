a658ec013154a7c11c5a4ad16e34d1c90f56b9e4
201702083 <201702083@o.cnu.ac.kr>
2022-03-04 00:33:17 +0900

    :white_check_mark: Mysql pool query test

--- src/controllers/ExampleController.ts
+++ src/controllers/ExampleController.ts
@@ -1,7 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
+import { logger } from '../utils/logger';
+
+const pool = require('../models/InitiateMysqlPool');
 
 const exampleControll = async (req: Request, res: Response) => {
-  //const returnData :object = await model1.basicQuery("SHOW STATUS LIKE 'Threads_connected';")
-  res.send('example');
+  const test = await pool.connect(async (con: any) => {
+    const result = await con.query(`show tables`, []);
+    // ...비지니스로직...
+    return result;
+  })();
+  res.status(200).send(JSON.stringify(test));
 };
-export default { exampleControll };
+const exampleTransaction = async (req: Request, res: Response) => {
+  await pool.transaction((con: any) =>
+    con.query(`insert into manager values("myEmail","myPW",0,"myPubkey",0)`),
+  )();
+  res.status(200).send();
+};
+export default { exampleControll, exampleTransaction };
