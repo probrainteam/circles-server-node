d8ca96ec17a0a50f9abeebac6ebedc47bbce103f
201702083 <201702083@o.cnu.ac.kr>
2022-03-04 00:37:31 +0900

    :wastebasket: Clean up deprecated connection loader

--- src/loaders/mysql.ts
+++ src/loaders/mysql.ts
@@ -1,4 +1,3 @@
-const mysql = require('../models/InitiateMysqlEnvironment');
 const pool = require('../models/InitiateMysqlPool');
 export default async (): Promise<any> => {
   return await pool.getPool();
--- src/models/DtoModel.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractDtoModel } from './AbstractDtoModel';
-
-class DtoModel extends AbstractDtoModel {
-  constructor() {
-    super();
-    this.getConnector().destroy();
-  }
-}
-export { DtoModel };
